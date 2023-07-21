815b16f656db3fc69e43906f9c85ebb6c273fe05
Miguel0223 <miglopez23@gmail.com>
2023-07-20 17:42:27 -0700

    YTDownloader
    Download any video with highest possible quality

---
+++ YTDownload.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+from sys import argv
+
+
+
+input = str(input("Enter the link of the video you want to download: "))
+
+
+link = input
+
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("Views: ", yt.views)
+print("Length of video: ", yt.length, "seconds")
+
+
+print("Downloading.....")
+
+yd = yt.streams.get_highest_resolution()
+yd.download('Downloads')
+
+print("Download completed!!")
